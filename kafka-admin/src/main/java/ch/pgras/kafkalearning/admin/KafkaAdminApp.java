35e0bb3dc5915acff26d70fe71aa923fb7ef76bf
Gras Patrick <pkg@elcaNet.local>
2019-02-14 13:03:49 +0100

    more structure

---
+++ kafka-admin/src/main/java/ch/pgras/kafkalearning/admin/KafkaAdminApp.java
@@ -0,0 +1,43 @@
+package ch.pgras.kafkalearning.admin;
+
+import kafka.admin.AdminUtils;
+import kafka.admin.RackAwareMode;
+import kafka.utils.ZKStringSerializer$;
+import kafka.utils.ZkUtils;
+import org.I0Itec.zkclient.ZkClient;
+import org.I0Itec.zkclient.ZkConnection;
+
+import java.util.Properties;
+
+public class KafkaAdminApp {
+
+    public static void main(String[] args) {
+        String zookeeperConnect = "zkserver1:2181,zkserver2:2181";
+        int sessionTimeoutMs = 10 * 1000;
+        int connectionTimeoutMs = 8 * 1000;
+
+        String topic = "my-topic";
+        int partitions = 2;
+        int replication = 3;
+        Properties topicConfig = new Properties(); // add per-topic configurations settings here
+
+        // Note: You must initialize the ZkClient with ZKStringSerializer.  If you don't, then
+        // createTopic() will only seem to work (it will return without error).  The topic will exist in
+        // only ZooKeeper and will be returned when listing topics, but Kafka itself does not create the
+        // topic.
+        ZkClient zkClient = new ZkClient(
+                zookeeperConnect,
+                sessionTimeoutMs,
+                connectionTimeoutMs,
+                ZKStringSerializer$.MODULE$);
+
+        // Security for Kafka was added in Kafka 0.9.0.0
+        boolean isSecureKafkaCluster = false;
+
+        // org.apache.kafka.clients.admin.AdminClient
+
+        ZkUtils zkUtils = new ZkUtils(zkClient, new ZkConnection(zookeeperConnect), isSecureKafkaCluster);
+        AdminUtils.createTopic(zkUtils, topic, partitions, replication, topicConfig, RackAwareMode.Enforced$.MODULE$);
+        zkClient.close();
+    }
+}
